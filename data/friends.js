1f93e407036744e5a9fdd8520249475731326236
Megan <mhmeluskey@gmail.com>
2018-11-01 17:52:09 -0400

    everything but matching

---
+++ data/friends.js
@@ -0,0 +1,22 @@
+var friendsArray = [
+    {
+    name: "Ahmed",
+    pic: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5",
+    ]
+    },
+  ];
+  
+ 
+  module.exports = friendsArray;
+  
